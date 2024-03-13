1c3ba2327a4b3cd9b5c4cf78fbeaa6a4c56cb5d5
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-03-13 15:30:16 +0100

    started working on the header

--- src/modules/shared/components/customer/customerHeader/Header.tsx
+++ src/modules/shared/components/customer/customerHeader/Header.tsx
@@ -1,5 +1,18 @@
+import ShoppingBagIcon from '../../../assets/icons/customerLayout/shoppingBag.svg';
+import ProfileIcon from '../../../assets/icons/customerLayout/profileIcon.svg';
+import MagnifyingGlass from '../../../assets/icons/customerLayout/MagnifyingGlass.svg';
+
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <img src={ShoppingBagIcon} alt="" />
+      <img src={ProfileIcon} alt="" />
+      <img src={MagnifyingGlass} alt="" />
+      {/* <ShoppingBagIcon />
+      <ProfileIcon />
+      <MagnifyingGlass /> */}
+    </div>
+  );
 }
 
 export default Header;
