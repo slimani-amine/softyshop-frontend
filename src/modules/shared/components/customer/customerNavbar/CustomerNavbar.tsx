60e83ca4a27b4512810d24f8e11df09677f98dd9
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-03-15 09:59:06 +0100

    started working on the navbar

--- src/modules/shared/components/customer/customerNavbar/CustomerNavbar.tsx
+++ src/modules/shared/components/customer/customerNavbar/CustomerNavbar.tsx
@@ -1,15 +1,23 @@
-import chevronDownGray from '../../../assets/icons/customerLayout/Navbar/chevron-down-gray.svg';
 import chevronDownBlack from '../../../assets/icons/customerLayout/Navbar/chevron-down-black.svg';
 import chevronRightBlack from '../../../assets/icons/customerLayout/Navbar/chevron-right-black.svg';
 import categoriesIcon from '../../../assets/icons/customerLayout/Navbar/categories-icon.svg';
+import NavigationElement from './components/navigationElements/NavigationElement';
 
 function CustomerNavbar() {
   return (
     <div className="customer-navbar">
+      <div className="navigation-elements">
+        <NavigationElement>Home</NavigationElement>
+        <NavigationElement>Mega Menu</NavigationElement>
+        <NavigationElement>Full Screen Menu</NavigationElement>
+        <NavigationElement>Pages</NavigationElement>
+        <NavigationElement>User Account</NavigationElement>
+        <NavigationElement>Vendor Account</NavigationElement>
+      </div>
+
       {/* <img src={chevronDownBlack} alt="" /> */}
       <img src={categoriesIcon} alt="" />
       <img src={chevronRightBlack} alt="" />
-      <img src={chevronDownGray} height="20px" alt="" />
     </div>
   );
 }
