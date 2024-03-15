53d738ebe4ac8c9127adf9ec32c20ea8d4db113e
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-03-15 09:11:46 +0100

    header completed

--- src/modules/shared/components/customer/customerHeader/Header.tsx
+++ src/modules/shared/components/customer/customerHeader/Header.tsx
@@ -1,18 +1,27 @@
-import ShoppingBagIcon from '../../../assets/icons/customerLayout/Header/shoppingBag.svg';
-import ProfileIcon from '../../../assets/icons/customerLayout/Header/profileIcon.svg';
-import MagnifyingGlass from '../../../assets/icons/customerLayout/Header/MagnifyingGlass.svg';
-import Logo from '../../../assets/icons/customerLayout/Header/logo-complete.svg';
+import { ReactComponent as ShoppingBagIcon1 } from '../../../assets/icons/customerLayout/Header/shoppingBag.svg';
 
+import { ReactComponent as ProfileIcon1 } from '../../../assets/icons/customerLayout/Header/profileIcon.svg';
+import { ReactComponent as MagnifyingGlass1 } from '../../../assets/icons/customerLayout/Header/MagnifyingGlass.svg';
+import Logo from '../../../assets/icons/customerLayout/Header/logo-complete.svg';
 function Header() {
   return (
     <div className="header">
-      <img src={Logo} alt="" />
-      <img src={MagnifyingGlass} alt="" />
-      <img src={ProfileIcon} alt="" />
-      <img src={ShoppingBagIcon} alt="" />
-      {/* <ShoppingBagIcon />
-      <ProfileIcon />
-      <MagnifyingGlass /> */}
+      <div className="logo-wrapper">
+        <img src={Logo} alt="" />
+      </div>
+      <MagnifyingGlass1 />
+      <div className="icons">
+        <div className="profile-icon-wrapper">
+          <ProfileIcon1 className="profile-icon" />
+        </div>
+        <div className="shopping-bag-icon-wrapper">
+          <ShoppingBagIcon1 className="shopping-bag-icon" />
+        </div>
+
+        <span className="cart-items-number">
+          <p>3</p>
+        </span>
+      </div>
     </div>
   );
 }
