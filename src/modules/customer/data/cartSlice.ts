0195cdbe316e820429616d8471e0e0b2c54b98d8
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-03-21 08:54:00 +0100

    feat(cart&wishlist): Stopped Working on those 2 features and started working on the tasks of the 2nd sprint

--- src/modules/customer/data/cartSlice.ts
+++ src/modules/customer/data/cartSlice.ts
@@ -1,1 +1,18 @@
-// const addItemToCart = createSlice({});
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      if (true) console.log(state, action);
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export const { addToCart } = cartSlice.actions;
--- src/modules/shared/store/rootReducer.ts
+++ src/modules/shared/store/rootReducer.ts
@@ -3,11 +3,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import themeReducer from './slices/theme/themeSlice';
 import authReducer from '../../auth/data/authSlice';
 import roleReducer from '../../auth/data/roleSlice';
+import cartReducer from '@src/modules/customer/data/cartSlice';
 
 const rootReducer = combineReducers({
   theme: themeReducer,
   auth: authReducer,
   role: roleReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
