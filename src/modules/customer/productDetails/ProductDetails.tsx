90ac7e4c1e082be2d5ee6e51711a180338ae7cde
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-04-02 10:39:37 +0100

    feat(productDetails): successfully implemented the display of the productDetails

--- src/modules/customer/productDetails/ProductDetails.tsx
+++ src/modules/customer/productDetails/ProductDetails.tsx
@@ -1,16 +1,23 @@
 import { BASE_URL } from '@src/modules/auth/data/authThunk';
+import Button from '@src/modules/shared/components/Button/Button';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function ProductDetails() {
   const { productId } = useParams();
-  const [product, setProduct] = useState({
-    id: 0,
-    name: '',
-    image: '',
-    rating: '',
-    price: '',
-  });
+  const [product, setProduct] = useState([
+    {
+      id: 0,
+      name: '',
+      image: '',
+      rating: '',
+      price: 0,
+      brand_id: 0,
+      category_id: 0,
+      availability: 0,
+      store_id: 0,
+    },
+  ]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,8 +36,47 @@ function ProductDetails() {
 
     fetchData();
   }, [BASE_URL]);
+  const theProduct = product[0];
+  console.log(theProduct);
 
-  return <div>{product[0]?.name}</div>;
+  return (
+    <>
+      <div className="product-details">
+        <div className="image-wrapper">
+          <img
+            width={400}
+            height={400}
+            className=""
+            src={theProduct.image}
+            alt=""
+          />
+        </div>
+        <div className="product-info">
+          <h1 className="product-name">{theProduct.name}</h1>
+          <div className="brand-and-category">
+            <p className="brand">
+              <span className="brand-title">Brand:</span> {theProduct.brand_id}
+            </p>
+            <p className="brand">
+              <span className="brand-title">category:</span>{' '}
+              {theProduct.category_id}
+            </p>
+          </div>
+
+          {/* <p className='rate'>theProduct.rate</p> */}
+
+          <h2 className="price">${theProduct.price}</h2>
+          <p className="stock">
+            Stock {theProduct.availability === 1 ? 'Available' : 'Unavailable'}
+          </p>
+          <Button label={'Add To Cart'} />
+          <p className="store">
+            <span className="store-title">Sold By:</span> {theProduct.store_id}{' '}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProductDetails;
