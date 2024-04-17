40d26fe0f32613aa4d9c3c57c827a7c141cce2b2
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-04-17 08:08:24 +0100

    testing git

--- src/modules/shared/components/Sidebar/items.tsx
+++ src/modules/shared/components/Sidebar/items.tsx
@@ -1,64 +1,54 @@
 /* eslint-disable react-refresh/only-export-components */
-import { ReactComponent as ProductIcon } from "../../assets/icons/sidebar/product.svg";
-import { ReactComponent as CatergoryIcon } from "../../assets/icons/sidebar/catergory.svg";
-import { ReactComponent as BrandIcon } from "../../assets/icons/sidebar/brand.svg";
-import { ReactComponent as StoreIcon } from "../../assets/icons/sidebar/bookstore.svg";
-import { ReactComponent as VendorIcon } from "../../assets/icons/sidebar/vendor.svg";
+//hola hola hola
+import { ReactComponent as ProductIcon } from '../../assets/icons/sidebar/product.svg';
+import { ReactComponent as CatergoryIcon } from '../../assets/icons/sidebar/catergory.svg';
+import { ReactComponent as BrandIcon } from '../../assets/icons/sidebar/brand.svg';
+import { ReactComponent as StoreIcon } from '../../assets/icons/sidebar/bookstore.svg';
+import { ReactComponent as VendorIcon } from '../../assets/icons/sidebar/vendor.svg';
 
 export const SIDEBARITEMS = [
-  
   {
-    link: "/products",
-    label: "Products",
+    link: '/products',
+    label: 'Products',
     icon: <ProductIcon />,
-    roles : ["ADMIN" , "VENDOR"],
-    
+    roles: ['ADMIN', 'VENDOR'],
   },
   {
-    link: "/categories",
-    label: "Categories",
+    link: '/categories',
+    label: 'Categories',
     icon: <CatergoryIcon />,
-    roles : ["ADMIN","VENDOR"],
-   
+    roles: ['ADMIN', 'VENDOR'],
   },
 
   {
-    link:  "/stores",
-    label: "Stores",
+    link: '/stores',
+    label: 'Stores',
     icon: <StoreIcon />,
-    roles : ["ADMIN","VENDOR"],
-    
+    roles: ['ADMIN', 'VENDOR'],
   },
   {
-    link: "/vendors",
-    label: "Vendors",
+    link: '/vendors',
+    label: 'Vendors',
     icon: <VendorIcon />,
-    roles : ["ADMIN","VENDOR"],
-   
+    roles: ['ADMIN', 'VENDOR'],
   },
   {
-    link: "/brands",
-    label: "Brands",
+    link: '/brands',
+    label: 'Brands',
     icon: <VendorIcon />,
-    roles : ["VENDOR","ADMIN"],
+    roles: ['VENDOR', 'ADMIN'],
   },
 
-
-  
   {
-    link: "/creators",
-    label: "Creators",
+    link: '/creators',
+    label: 'Creators',
     icon: <BrandIcon />,
-    roles : ["VENDOR","ADMIN"]
-   
+    roles: ['VENDOR', 'ADMIN'],
   },
   {
-    link:  "/orders/create",
-    label: "Create Order",
+    link: '/orders/create',
+    label: 'Create Order',
     icon: <StoreIcon />,
-    roles : ["ADMIN","VENDOR"],
-    
+    roles: ['ADMIN', 'VENDOR'],
   },
-
-
 ];
