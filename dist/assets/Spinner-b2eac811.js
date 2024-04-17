acc4757ec792d409941f3046a5329c1f07bf98d0
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-04-17 08:37:05 +0100

    build: the control branch is built

--- dist/assets/Spinner-b2eac811.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-043bdbd9.js";function r(){return e.jsx("div",{className:"spinner-element"})}export{r as S};
--- dist/assets/Test-2d29f433.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-043bdbd9.js";const e=()=>s.jsx("div",{className:"dashboard",children:"Test"});export{e as default};
