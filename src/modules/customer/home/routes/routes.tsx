de65116a714f57c5c59e73020455ae076c7ebcaf
Montassar Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2024-05-03 08:43:52 +0100

    fix: fixed customer's permissions

--- src/modules/customer/home/routes/routes.tsx
+++ src/modules/customer/home/routes/routes.tsx
@@ -2,11 +2,7 @@ import { RouteProps } from 'react-router-dom';
 import { Fragment, lazy } from 'react';
 import AuthGuard from '@src/modules/shared/guards/AuthGuard';
 import CustomerLayout from '@src/modules/shared/layout/CustomerLayout/CustomerLayout';
-<<<<<<< HEAD
 import { ADMIN, VENDOR, CUSTOMER } from '@src/global_roles_config';
-=======
-import { ADMIN , CUSTOMER, VENDOR } from "@src/global_roles_config";
->>>>>>> ba64723d407e15ceae794ee5d7737fc28bb83b88
 
 type RouteConfig = {
   exact: boolean | null;
@@ -33,11 +29,7 @@ const routes: RouteConfig[] = [
     path: '/home/:storeId',
     component: lazy(() => import('../../storeDetails/storeDetails')),
     layout: CustomerLayout,
-<<<<<<< HEAD
     roles: [ADMIN, VENDOR, CUSTOMER],
-=======
-    roles: [ADMIN, VENDOR,CUSTOMER],
->>>>>>> ba64723d407e15ceae794ee5d7737fc28bb83b88
   },
   {
     exact: true,
@@ -45,11 +37,7 @@ const routes: RouteConfig[] = [
     path: '/home/:storeId/:productId',
     component: lazy(() => import('../../productDetails/ProductDetails')),
     layout: CustomerLayout,
-<<<<<<< HEAD
     roles: [ADMIN, VENDOR, CUSTOMER],
-=======
-    roles: [ADMIN, VENDOR,CUSTOMER],
->>>>>>> ba64723d407e15ceae794ee5d7737fc28bb83b88
   },
   {
     exact: true,
@@ -57,11 +45,7 @@ const routes: RouteConfig[] = [
     path: '/books/:productId',
     component: lazy(() => import('../../productDetails/ProductDetails')),
     layout: CustomerLayout,
-<<<<<<< HEAD
     roles: [ADMIN, VENDOR, CUSTOMER],
-=======
-    roles: [ADMIN, VENDOR,CUSTOMER],
->>>>>>> ba64723d407e15ceae794ee5d7737fc28bb83b88
   },
 ];
 
